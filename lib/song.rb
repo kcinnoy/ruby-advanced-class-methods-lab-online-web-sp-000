0c7cf2132f1a8ea0c4d2554c0d9f2cd59a7e1a52
Yonnick Brown <25hour24@gmail.com>
2019-02-15 17:41:58 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,7 +31,11 @@ class Song
   end
 
   def self.alphabetical
-      self.all.sort_by {|song| song.name}  
+      self.all.sort_by {|song| song.name}
+  end
+
+  def new_from_filename(name)
+
   end
 
 
