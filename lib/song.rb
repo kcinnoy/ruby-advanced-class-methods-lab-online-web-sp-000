08d4fc06f8024c573f529673b2b70471032235bf
Yonnick Brown <25hour24@gmail.com>
2019-02-15 17:15:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,30 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+
+  def self.create
+    song = self.new
+    song.save
+    return song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    return song
+  end
+
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    song.save
+    song
+  end
+
+  def self.find_by_name(name)
+    self.all.find{|song| song.class.name == name}
+  end
+
   def self.all
     @@all
   end
