5cfa41bb6468f0ce4cfcb0ba659ed7628c05b3c2
Yonnick Brown <25hour24@gmail.com>
2019-02-15 17:18:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,6 +26,11 @@ class Song
     self.all.find{|song| song.name == name}
   end
 
+  def self.find_or_create_by_name(name)
+    self.all.find{|song| song.name == name}
+  end
+
+
   def self.all
     @@all
   end
